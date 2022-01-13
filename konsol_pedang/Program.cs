a4c1b1b908c2e88812fad7d0f827bf16c7c806f2
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-13 11:43:28 +0800

    worked program NEXT: WPF version

--- konsol_pedang/Program.cs
+++ konsol_pedang/Program.cs
@@ -6,7 +6,29 @@ namespace konsol_pedang
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");
+            Random ngacak = new Random();
+            KerusakanPedang kped = new KerusakanPedang();
+
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, "+
+                    "3 for both, anything else to quit:");
+
+                char kunchee = Console.ReadKey().KeyChar;
+
+                if (kunchee != '0' && kunchee != '1' && kunchee != '2' && kunchee != '3') return;
+
+                int roll = ngacak.Next(1, 7) + ngacak.Next(1, 7) + ngacak.Next(1, 7);
+
+                kped.Roll = roll;
+                kped.AturKeajaiban (kunchee == '1' || kunchee == '3');
+                kped.AturNyalaApi (kunchee == '2' || kunchee == '4');
+
+
+                Console.WriteLine("\nRolled " + roll + " for " + kped.Kerusakan + " HP\n");
+            }
+
+
         }
     }
 }
